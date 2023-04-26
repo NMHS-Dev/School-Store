ddcb655ffc8ca8f9dbf905030130a95e55419e28
Program Director <Program_Director@NMHSCourses.us>
2023-04-26 15:45:09 -0600

    initiall changes

---
+++ src/pages/about.js
@@ -0,0 +1,17 @@
+// Step 1: Import React
+import * as React from 'react'
+import {link} from 'gatsby'
+
+// Step 2: Define your component
+const AboutPage = () => {
+  return (
+    <main>
+      <h1>About Our Program</h1>
+      <Link to="/">Back to Home</Link>
+      <p>Learn about the research our program has been doing, what models we have used as frameworks and what our end goal is</p>
+    </main>
+  )
+}
+
+// Step 3: Export your component
+export default AboutPage
